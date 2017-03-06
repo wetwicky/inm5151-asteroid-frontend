538da19ce464d22709e2d57b1d008e747027da51
Marc-Antoine Sauve <madeinqchw@gmail.com>
2017-03-06 15:15:33 -0500

    Remove unnecessary files
    Signed-off-by: Marc-Antoine Sauve <madeinqchw@gmail.com>

--- deps/rethinkdb/lib/rethinkdb/q.ex
+++
@@ -1,9 +0,0 @@
-defmodule RethinkDB.Q do
-  @moduledoc false
-  defstruct query: nil
-end
-defimpl Poison.Encoder, for: RethinkDB.Q do
-  def encode(%{query: query}, options) do
-    Poison.Encoder.encode(query, options)
-  end
-end
