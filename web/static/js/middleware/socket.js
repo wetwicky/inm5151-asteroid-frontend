05fdd9621918dd1032f44ff526e33f18583420ff
Marc-Antoine Sauve <madeinqchw@gmail.com>
2017-03-13 15:08:17 -0400

    Remove unused files and remove react game kit
    Signed-off-by: Marc-Antoine Sauve <madeinqchw@gmail.com>

--- web/static/js/middleware/socket.js
+++ web/static/js/middleware/socket.js
@@ -14,7 +14,6 @@ const socketMiddleware = (function () {
         channel.join()
             .receive('ok', resp => {
                 // Tell the store we are connected
-                console.log("Ok: ", resp)
                 store.dispatch(connected())
             })
             .receive('error', resp => {
